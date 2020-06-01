d8ffa980a9390ef5a96ae7322d831dc1691395a1
Shiv-13 <56431311+Shiv-13@users.noreply.github.com>
2020-06-02 01:41:18 +0530

    Update BasicArithmeticCode.c

--- BasicArithmeticCode.c
+++ BasicArithmeticCode.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int a=60,b=20;
-int Sum=0;
+int Sum=0, Diff=0;
 
 int main()
 {
 Sum= a+b;
 printf("Sum of two numbers = %d\n",Sum);
+
+Diff=a-b;
+printf("Difference of two numbers = %d\n",Diff);
+
 
 return 0;
 }
